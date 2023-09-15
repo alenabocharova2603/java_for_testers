4270aa71f603bfff12744e88db34f15b10a8d761
alenabocharova2603 <alenabocharova2603@gmail.com>
2023-09-15 13:49:41 +0200

    Эксперименты с функциями и часть домашней работы

---
+++ sandbox/src/main/java/Triangle.java
@@ -0,0 +1,11 @@
+public class Triangle {
+    public static void main(String[] args) {
+        var Side_A = 3;
+        var Side_B = 5;
+        var Side_C = 10;
+        System.out.println("Длина стороны " + "a " + "= " + Side_A);
+        System.out.println("Длина стороны " + "b " + "= " + Side_B);
+        System.out.println("Длина стороны " + "c " + "= " + Side_C);
+        System.out.println("Периметр треугольника " + "= " + (Side_A+Side_B+Side_C));
+    }
+}

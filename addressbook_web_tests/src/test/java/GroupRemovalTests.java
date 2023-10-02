8ad09b1843383451c847971162034b7612cda80c
alenabocharova2603 <alenabocharova2603@gmail.com>
2023-10-02 11:40:54 +0200

    Фикстура и предусловия для создания групп

--- addressbook_web_tests/src/test/java/GroupRemovalTests.java
+++ addressbook_web_tests/src/test/java/GroupRemovalTests.java
@@ -3,35 +3,47 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GroupRemovalTests {
-    private WebDriver driver;
+    private static WebDriver driver;
 
     @BeforeEach
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "C:\\Chrome\\chromedriver.exe");
         driver = new ChromeDriver();
-
-    }
-
-    @AfterEach
-    public void tearDown() {
-        driver.quit();
-    }
-
-    @Test
-    public void remove() {
         driver.get("http://localhost/addressbook/");
         driver.manage().window().setSize(new Dimension(1054, 808));
         driver.findElement(By.name("user")).sendKeys("admin");
         driver.findElement(By.name("pass")).sendKeys("secret");
         driver.findElement(By.cssSelector("input:nth-child(7)")).click();
-        driver.findElement(By.linkText("groups")).click();
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.findElement(By.linkText("Logout")).click();
+        driver.quit();
+    }
+
+    @Test
+    public void canRemoveGroup() {
+        if (!isElementPresent(By.name("new"))) {
+            driver.findElement(By.linkText("groups")).click();
+        }
         driver.findElement(By.name("selected[]")).click();
         driver.findElement(By.cssSelector("input:nth-child(11)")).click();
         driver.findElement(By.linkText("group page")).click();
-        driver.findElement(By.linkText("Logout")).click();
+
+    }
+    private boolean isElementPresent(By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException exception) {
+            return false;
+        }
     }
 }

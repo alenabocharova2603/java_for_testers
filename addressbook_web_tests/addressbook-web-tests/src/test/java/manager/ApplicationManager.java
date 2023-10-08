036d0ec4f62e29e8f9d180ae41785114b55d3f0c
alenabocharova2603 <alenabocharova2603@gmail.com>
2023-10-08 15:38:58 +0200

    Тесты в разных браузерах

--- addressbook_web_tests/addressbook-web-tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/addressbook-web-tests/src/test/java/manager/ApplicationManager.java
@@ -13,11 +13,17 @@ public class ApplicationManager {
     private GroupHelper groups;
     private ContactHelper contacts;
 
-    public void init() {
+    public void init(String browser) {
         System.setProperty("webdriver.chrome.driver", "C:\\Chrome\\chromedriver.exe");
         System.setProperty("webdriver.gecko.driver", "C:\\Firefox\\geckodriver.exe");
-        driver = new ChromeDriver();
-        driver = new FirefoxDriver();
+        if (driver == null) {
+            if ("firefox".equals(browser)) {
+                driver = new FirefoxDriver();
+            } else if ("chrome".equals(browser))
+                    driver = new ChromeDriver();
+        } else {
+            throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
+        }
         Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
         driver.get("http://localhost/addressbook/");
         driver.manage().window().setSize(new Dimension(550, 693));

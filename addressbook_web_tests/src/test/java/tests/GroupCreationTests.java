ad90528c4eb78c2b0232d5e92bf6b56b1936d625
alenabocharova2603 <alenabocharova2603@gmail.com>
2023-10-14 18:57:36 +0200

    Цикл со счетчиком

--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -30,8 +30,21 @@ public class GroupCreationTests extends TestBase {
     public void CanCreateMultipleGroup() {
         int n = 5;
         int groupCount = app.groups().getCount();
+
         app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
         int newGroupCount = app.groups().getCount();
         Assertions.assertEquals(groupCount + 1, newGroupCount);
+    }
+
+    @Test
+    public void CanCreateMultipleGroups() {
+        int n = 5;
+        int groupCount = app.groups().getCount();
+        for (int i = 0; i < n; i++) {
+            app.groups().createGroup(new GroupData(randomString(i*10), "group header", "group footer"));
+        }
+
+        int newGroupCount = app.groups().getCount();
+        Assertions.assertEquals(groupCount + n, newGroupCount);
     }
 }
--- addressbook_web_tests/src/test/java/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/tests/TestBase.java
@@ -3,6 +3,8 @@ package tests;
 import manager.ApplicationManager;
 import org.junit.jupiter.api.BeforeEach;
 
+import java.util.Random;
+
 public class TestBase {
     
     protected static ApplicationManager app;
@@ -17,5 +19,12 @@ public class TestBase {
         }
 
     }
-
+    public String randomString(int n) {
+        var rnd = new Random();
+        var result = "";
+        for (int i = 0; i < n; i++) {
+            result = result + (char)('a' + rnd.nextInt(26));
+        }
+        return result;
+    }
 }

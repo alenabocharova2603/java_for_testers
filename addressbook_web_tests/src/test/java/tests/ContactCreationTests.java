fdfc6223f4a5ee2e2fa0ffc0f96805a3e491a8ab
alenabocharova2603 <alenabocharova2603@gmail.com>
2023-10-17 15:47:59 +0200

    Параметризация тестов для контактов

--- addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
@@ -3,35 +3,56 @@ package tests;
 import model.ContactData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContactCreationTests extends TestBase {
 
-    @Test
-    public void createContact() {
+    public static List<ContactData> contactProvider() {
+        var result = new ArrayList<ContactData>();
+        for (var firstname : List.of("","contact firstname")) {
+            for (var lastname : List.of("", "contact lastname")) {
+               for (var address : List.of("","contact address")) {
+                   for (var mobile : List.of("","contact mobile")) {
+                       for (var email : List.of("","contact email")) {
+                           result.add(new ContactData(firstname,lastname,address,mobile,email));
+                       }
+                   }
+               }
+            }
+        }
+        for (int i = 0; i < 5; i++) {
+            result.add(new ContactData(randomString(i * 10),randomString(i * 10),randomString(i * 10),randomString(i * 10),randomString(i * 10)));
+        }
+        return result;
+    }
+
+    @ParameterizedTest
+    @MethodSource("contactProvider")
+    public void canCreateMultipleContacts(ContactData contact) {
         int contactCount = app.contacts().getContactCount();
-        app.contacts().createContact(new ContactData("Nina", "Ivanova", "3 Internacounal, 243", "+98567841456", "nina_kot@koler.com"));
+        app.contacts().createContact(contact);
         int newContactCount = app.contacts().getContactCount();
         Assertions.assertEquals(contactCount + 1, newContactCount);
         app.contacts().openHomePage();
     }
 
-
-    @Test
-    public void createContactWithFirstnameOnly() {
-        app.contacts().createContact(new ContactData().withFirstname("some name"));
-        app.contacts().openHomePage();
+    public static List<ContactData>  negativeContactProvider() {
+        var result = new ArrayList<ContactData>(List.of(
+                new ContactData("firstname' ","","","","")));
+        return result;
     }
-    @Test
-    public void canCreateMultipleContacts() {
-        int n = 5;
+
+    @ParameterizedTest
+    @MethodSource("negativeContactProvider")
+    public void canNotCreateContacts(ContactData contact) {
         int contactCount = app.contacts().getContactCount();
-
-        for (int i = 0; i < n; i++) {
-            app.contacts().createContact(new ContactData(randomString(i * 10), "Ivanova", "3 Internacounal, 243", "+98567841456", "nina_kot@koler.com"));
-        }
-
+        app.contacts().createContact(contact);
         int newContactCount = app.contacts().getContactCount();
-        Assertions.assertEquals(contactCount + n, newContactCount);
+        Assertions.assertEquals(contactCount, newContactCount);
         app.contacts().openHomePage();
     }
 }

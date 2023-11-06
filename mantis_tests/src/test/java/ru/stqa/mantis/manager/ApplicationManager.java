1666c5ec2f502e5aa65961272f8035952c16b99a
alenabocharova2603 <alenabocharova2603@gmail.com>
2023-11-06 10:04:32 +0100

    Задание 17_Добавлен модуль mantis

---
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
@@ -0,0 +1,48 @@
+package ru.stqa.mantis.manager;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+    private WebDriver driver;
+    private String string;
+    private Properties properties;
+    private SessionHelper sessionHelper;
+
+    public void init(String browser, Properties properties) {
+        this.string = browser;
+        this.properties = properties;
+        System.setProperty("webdriver.chrome.driver", "C:\\Chrome\\chromedriver.exe");
+        System.setProperty("webdriver.gecko.driver", "C:\\Firefox\\geckodriver.exe");
+
+    }
+
+    public WebDriver driver() {
+        if (driver == null) {
+            if ("firefox".equals(string)) {
+                driver = new FirefoxDriver();
+            } else if ("chrome".equals(string)) {
+                driver = new ChromeDriver();
+        } else {
+            throw new IllegalArgumentException(String.format("Unknown browser %s", string));
+        }
+            driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
+            driver.get(properties.getProperty("web.baseUrl"));
+            driver.manage().window().setSize(new Dimension(550, 693));
+        }
+        return driver;
+    }
+
+    public SessionHelper session() {
+        if (sessionHelper == null) {
+            sessionHelper = new SessionHelper(this);
+        }
+        return sessionHelper;
+    }
+}

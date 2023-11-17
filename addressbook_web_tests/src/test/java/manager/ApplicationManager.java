e41dbdb6098dd5a8a940a5dde9d9f582add22977
alenabocharova2603 <alenabocharova2603@gmail.com>
2023-11-17 15:03:53 +0100

    Лекция_10_5

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -5,8 +5,13 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -20,16 +25,26 @@ public class ApplicationManager {
     private HibernateHelper hbm;
 
 
-    public void init(String browser, Properties properties) {
+    public void init(String browser, Properties properties) throws MalformedURLException {
         this.properties = properties;
         System.setProperty("webdriver.chrome.driver", "C:\\Chrome\\chromedriver.exe");
         System.setProperty("webdriver.gecko.driver", "C:\\Firefox\\geckodriver.exe");
 
         if (driver == null) {
+            var seleniumServer = properties.getProperty("seleniumServer");
             if ("firefox".equals(browser)) {
-                driver = new FirefoxDriver();
-            } else if ("chrome".equals(browser))
-                driver = new ChromeDriver();
+               if (seleniumServer != null) {
+                   driver= new RemoteWebDriver(new URL(seleniumServer), new FirefoxOptions());
+               } else {
+                   driver = new FirefoxDriver();
+               }
+            } else if ("chrome".equals(browser)) {
+                if (seleniumServer != null) {
+                    driver= new RemoteWebDriver(new URL(seleniumServer), new ChromeOptions());
+                } else {
+                    driver = new ChromeDriver();
+                }
+            }
         } else {
             throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
         }
